6e1abff4a5c835a347db8964f335c66d3c600039
zhiyan114 <zhiyan114@zhiyan114.com>
2023-11-30 20:07:37 -0500

    Remove sentry - unsupported

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <esp_log.h>
 
+void setupSentry();
+void setup_wlan();
+void main_loader();
+
 extern "C" {
     void app_main() {
-        ESP_LOGI("main", "Hello world!");
+        // Sentry should be the first to load, but it doesn't work w/o wifi so...
+        setup_wlan();
+        main_loader();
     }
+}
+
+
+// This function setups the wifi connection
+void setup_wlan() {
+    
+}
+
+// This function should load the main code
+void main_loader() {
+    ESP_LOGI("main", "Hello world!");
 }
